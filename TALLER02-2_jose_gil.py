35ffca6567face64e3f2c85fc9cc6c24f73a0dcf
Jose David Gil <110634698+josedavidgil@users.noreply.github.com>
2022-09-27 13:51:32 -0500

    Create TALLER02-2_jose_gil.py

---
+++ TALLER02-2_jose_gil.py
@@ -0,0 +1,39 @@
+#TODO: iniciar y crear el arbol con una profundidad de almenos 4 con su familia (0.2)
+#TODO: validar la función de busqueda (0.2)
+'''
+Ejecutar la siguiente línea de código en la consola de python.
+p_01=persona('Jose','Smith Samuel','Maria Vicente')
+p_01.f_busqueda_Secuencial('Jose')   #Jose puede ser cambiado por algun otro integrante
+
+RESULTADO:
+('Jose', 'fu� encontrado(a), es la persona principal y se encuentra en la altura', 0)
+'''
+
+#TODO: validad la encriptación de los archivos (0.2)
+'''
+LA SOLUCION ESTA JUNTO AL PUNTO SIGUIENTE (DECRIPTACION)
+'''
+#TODO: validar la decriptación de archivos (0.2)
+'''
+Ejecutar la siguiente línea de código en la consola de python.
+p_01=persona('Jose','Smith Samuel','Maria Vicente')
+p_01.encript('prueba')
+p_01.decrip('prueba')
+
+RESULTADO:
+Jose
+['Maria', 'Vicente']
+['Smith', 'Samuel']
+'Arbol creado'
+'''
+#TODO: validar la converción a diccionario (0.2)
+'''
+Ejecutar la siguiente línea de código en la consola de python.
+p_01=persona('Jose','Smith Samuel','Maria Vicente')
+p_01.tree2dict()
+
+RESULTADO:
+{'Persona principal': 'Jose', 'Jose': 'Persona principal', 'Hermanos': ['Smith', 'Samuel'], 'Padres': ['Maria', 'Vicente'], 'Smith': 'Hermano(a)', 'Samuel': 'Hermano(a)', 'Maria': 'Padre/Madre', 'Vicente': 'Padre/Madre'}
+'''
+
+#NOTA ESTUDIANTE: La validación se ha hecho, tengo una duda, tiene que ser presentada de forma presencial en la clase?. Gracias
